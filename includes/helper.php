a22aacf2dffc356486cfbcf0edfb1de8fbe15c95
Moktadir Rahman <autocircled@gmail.com>
2022-06-11 14:24:08 +0600

    working ok

--- includes/helper.php
+++ includes/helper.php
@@ -62,6 +62,7 @@ class Helper {
 	public function woocommerce_catalog_mode() : void {
         remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
         remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+        remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 	}
 
     public function preload_contents() {}
